270ca3c8201ef5b79835a37a078f11f0ddacec88
zero.dev <zero.dev@kakaoenterprise.com>
2021-10-02 19:10:13 +0900

    [#1] build.gradle(project) -> kts 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31"
+        classpath(Dependencies.androidGradlePlugin)
+        classpath(Dependencies.Kotlin.gradlePlugin)
+        classpath(Dependencies.Dagger.hiltGradlePlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
