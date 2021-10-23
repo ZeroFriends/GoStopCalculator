65054cd93b981f8d2e37e64629ed367e783c4864
zozero94 <android_develop@kakao.com>
2021-10-23 18:29:22 +0900

    [#8] weak HashMap -> 일반적인 hashMap으로 변경

--- data/src/main/java/zero/friends/data/repository/PlayerRepositoryImpl.kt
+++ data/src/main/java/zero/friends/data/repository/PlayerRepositoryImpl.kt
@@ -9,11 +9,11 @@ import zero.friends.data.entity.PlayerEntity.Companion.toPlayer
 import zero.friends.data.source.dao.PlayerDao
 import zero.friends.domain.model.Player
 import zero.friends.domain.repository.PlayerRepository
-import java.util.*
+
 class PlayerRepositoryImpl(
     private val playerDao: PlayerDao,
 ) : PlayerRepository {
-    private val playerIdCache = WeakHashMap<Long, Int>()
+    private val playerIdCache = mutableMapOf<Long, Int>()
 
     override suspend fun addAutoGeneratePlayer(gameId: Long) {
         CoroutineScope(Dispatchers.IO).launch {
