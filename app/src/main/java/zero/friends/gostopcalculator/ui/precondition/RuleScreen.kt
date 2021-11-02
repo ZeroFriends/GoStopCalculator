990c4dfdd84c7a6e70c9b5d75277ce34623fdbd8
zozero94 <android_develop@kakao.com>
2021-11-02 20:19:49 +0900

    [#5] Rule 화면 작업 -> LazyColumn

--- app/src/main/java/zero/friends/gostopcalculator/ui/precondition/RuleScreen.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/precondition/RuleScreen.kt
@@ -3,15 +3,15 @@ package zero.friends.gostopcalculator.ui.precondition
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
@@ -19,25 +19,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import zero.friends.domain.model.Rule
 import zero.friends.gostopcalculator.R
 import zero.friends.gostopcalculator.ui.common.AprilBackground
 import zero.friends.gostopcalculator.ui.common.CenterTextTopBar
+import zero.friends.gostopcalculator.ui.common.NumberTextField
 import zero.friends.gostopcalculator.ui.common.SubActionOutLineButton
+
 
 sealed class RuleClickEvent {
     object Back : RuleClickEvent()
     object Complete : RuleClickEvent()
     object Helper : RuleClickEvent()
 }
+
 @Composable
 fun RuleScreen(ruleViewModel: RuleViewModel = hiltViewModel(), onBack: () -> Unit) {
     val scaffoldState = rememberScaffoldState()
+    val uiState by ruleViewModel.getUiState().collectAsState()
 
     BackHandler(true) {
         onBack()
     }
 
-    RuleScreen(scaffoldState) { ruleClickEvent ->
+    RuleScreen(scaffoldState, uiState) { ruleClickEvent ->
         when (ruleClickEvent) {
             RuleClickEvent.Back -> onBack()
             RuleClickEvent.Complete -> TODO()
@@ -48,6 +53,7 @@ fun RuleScreen(ruleViewModel: RuleViewModel = hiltViewModel(), onBack: () -> Uni
 @Composable
 fun RuleScreen(
     scaffoldState: ScaffoldState,
+    uiState: RuleUiState,
     clickEvent: (RuleClickEvent) -> Unit,
 ) {
     Scaffold(
@@ -72,22 +78,61 @@ fun RuleScreen(
             Column {
                 TitleOutlinedTextField(
                     title = "규칙이름",
-                    hint = "",
+                    hint = uiState.currentTime,
                     initialText = ""
                 ) { textFieldValue.value = it }
 
-                RuleLazyColumn(clickEvent)
+                RuleLazyColumn(uiState.rules, clickEvent)
             }
         }
     }
 }
 
 @Composable
-fun RuleLazyColumn(clickEvent: (RuleClickEvent) -> Unit) {
+fun RuleLazyColumn(rules: Rule, clickEvent: (RuleClickEvent) -> Unit) {
     LazyColumn(contentPadding = PaddingValues(top = 35.dp, bottom = 12.dp)) {
         item {
             AmountSettingBlock { clickEvent(RuleClickEvent.Helper) }
         }
+        itemsIndexed(rules.spread()) { index, rule ->
+            RuleItem(index = index)
+        }
+
+    }
+}
+
+@Composable
+fun RuleItem(index: Int) {
+    Row(
+        Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = (index + 1).toString(),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp),
+                fontSize = 16.sp,
+                color = colorResource(id = R.color.orangey_red))
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 6.dp)
+            ) {
+                Text(
+                    text = "RuleName",
+                    fontSize = 16.sp,
+                    color = colorResource(id = R.color.nero)
+                )
+                Text(text = "필수 항목입니다.", fontSize = 10.sp, color = colorResource(id = R.color.gray38))
+            }
+        }
+        
+        NumberTextField(modifier = Modifier.weight(1f), "원")
     }
 }
 
@@ -106,5 +151,5 @@ fun AmountSettingBlock(modifier: Modifier = Modifier, onHelperClick: () -> Unit)
 @Preview
 @Composable
 fun RuleScreenPreview() {
-    RuleScreen(rememberScaffoldState()) {}
+    RuleScreen(rememberScaffoldState(), uiState = RuleUiState()) {}
 }
--- app/src/main/java/zero/friends/gostopcalculator/ui/precondition/RuleViewModel.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/precondition/RuleViewModel.kt
@@ -2,9 +2,22 @@ package zero.friends.gostopcalculator.ui.precondition
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import zero.friends.domain.model.Rule
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+
+data class RuleUiState(
+    val currentTime: String = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(System.currentTimeMillis()),
+    val rules: Rule = Rule(),
+)
 
 @HiltViewModel
 class RuleViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(RuleUiState())
+    fun getUiState() = _uiState.asStateFlow()
 
 }
