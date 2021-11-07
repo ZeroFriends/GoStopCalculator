37e7e9f808e44c7d16fddfa0c00fc160695f3d38
zozero94 <android_develop@kakao.com>
2021-11-08 01:56:58 +0900

    [#5] 4인 이상일 경우만 광팔기 옵션 보여주기

--- app/src/main/java/zero/friends/gostopcalculator/di/AppModule.kt
+++ app/src/main/java/zero/friends/gostopcalculator/di/AppModule.kt
@@ -38,6 +38,8 @@ class AppModule {
 
     @Provides
     fun provideGetDefaultRuleUseCase(
+        gameRepository: GameRepository,
+        playerRepository: PlayerRepository,
         ruleRepository: RuleRepository,
-    ): GetDefaultRuleUseCase = GetDefaultRuleUseCase(ruleRepository)
+    ): GetDefaultRuleUseCase = GetDefaultRuleUseCase(gameRepository, playerRepository, ruleRepository)
 }
--- domain/src/main/java/zero/friends/domain/usecase/GetDefaultRuleUseCase.kt
+++ domain/src/main/java/zero/friends/domain/usecase/GetDefaultRuleUseCase.kt
@@ -1,10 +1,23 @@
 package zero.friends.domain.usecase
 
 import zero.friends.domain.model.Rule
+import zero.friends.domain.repository.GameRepository
+import zero.friends.domain.repository.PlayerRepository
 import zero.friends.domain.repository.RuleRepository
 
-class GetDefaultRuleUseCase(private val ruleRepository: RuleRepository) {
-    suspend operator fun invoke() :List<Rule>{
-        return ruleRepository.getDefaultRule()
+class GetDefaultRuleUseCase(
+    private val gameRepository: GameRepository,
+    private val playerRepository: PlayerRepository,
+    private val ruleRepository: RuleRepository,
+) {
+    suspend operator fun invoke(): List<Rule> {
+        val gameId = gameRepository.getCurrentGameId()
+
+        val canSellShine = playerRepository.getPlayers(gameId).size > 3
+        val rules = ruleRepository.getDefaultRule().toMutableList()
+        val sellShineRule = rules.find { it.title == "광팔기" }
+
+        if (!canSellShine) rules.remove(sellShineRule)
+        return rules.toList()
     }
 }
