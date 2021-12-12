e6055408f2e98d6fca8b07eb303391146c1179e3
zozero94 <android_develop@kakao.com>
2021-12-12 23:47:35 +0900

    [#9] PlayerPickItem UI 작업

--- app/src/main/java/zero/friends/gostopcalculator/ui/board/PrepareScreen.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/board/PrepareScreen.kt
@@ -1,21 +1,20 @@
 package zero.friends.gostopcalculator.ui.board
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -102,7 +101,12 @@ private fun PlayerPickList(modifier: Modifier = Modifier, players: List<Player> 
     Column(
         modifier = modifier.fillMaxWidth()
     ) {
-        Text(text = stringResource(R.string.player_list))
+        Text(
+            text = stringResource(R.string.player_list),
+            fontSize = 16.sp,
+            color = colorResource(id = R.color.nero),
+            fontWeight = FontWeight.Bold
+        )
         Spacer(modifier = Modifier.padding(9.dp))
         LazyColumn {
             itemsIndexed(players) { index: Int, item: Player ->
@@ -112,11 +116,47 @@ private fun PlayerPickList(modifier: Modifier = Modifier, players: List<Player> 
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-private fun PlayerPickItem(index: Int, item: Player) {
-    TODO("Not yet implemented")
+private fun PlayerPickItem(index: Int, item: Player, isCheck: Boolean = true, onClick: (Boolean) -> Unit = {}) {
+    val modifier = if (isCheck) Modifier.background(
+        color = Color(R.color.orangey_red).copy(alpha = 0.1f),
+        RoundedCornerShape(18.dp)
+    )
+    else Modifier
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(18.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row {
+            Text(
+                text = (index + 1).toString(),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.orangey_red)
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = item.name, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        }
+        Surface(onClick = { onClick(!isCheck) }) {
+            if (isCheck) {
+                Image(painter = painterResource(id = R.drawable.ic_check_circle_24_dp), contentDescription = null)
+            } else {
+                Image(painter = painterResource(id = R.drawable.ic_unchecked_circle_24_dp), contentDescription = null)
+            }
+        }
+    }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun PickItemPreview() {
+    PlayerPickItem(index = 1, item = Player(1, "zero"))
+}
 
 @Preview
 @Composable
