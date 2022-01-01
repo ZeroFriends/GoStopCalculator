c8709214eaa6163e1772281de892e82f2dc2ce73
zozero94 <android_develop@kakao.com>
2022-01-01 22:11:00 +0900

    rule update

--- app/src/main/java/zero/friends/gostopcalculator/Const.kt
+++
@@ -1,6 +0,0 @@
-package zero.friends.gostopcalculator
-
-object Const {
-    const val GameId = "gameId"
-    const val RoundId = "roundId"
-}
---
+++ domain/src/main/java/zero/friends/domain/util/Const.kt
@@ -0,0 +1,22 @@
+package zero.friends.gostopcalculator
+
+object Const {
+    const val GameId = "gameId"
+    const val RoundId = "roundId"
+
+    object Rule {
+        const val Winner = "승자"
+        const val Sell = "광팜"
+
+        const val President = "총통"
+        const val FiveShine = "5광"
+        const val FirstFuck = "첫 뻑"
+        const val ThreeFuck = "쓰리 뻑"
+        const val FirstDdadak = "첫 따닥"
+
+        const val PeaBak = "피 박"
+        const val LightBak = "광 박"
+        const val MongBak = "멍 박"
+        const val GoBack = "고 박"
+    }
+}
