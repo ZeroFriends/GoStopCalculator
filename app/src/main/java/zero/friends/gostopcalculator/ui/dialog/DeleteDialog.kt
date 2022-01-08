4444a83084c2f60c1f14e3484bb51fabe6fba276
zozero94 <android_develop@kakao.com>
2022-01-08 12:17:05 +0900

    삭제 다이어로그 버튼배경 제거

--- app/src/main/java/zero/friends/gostopcalculator/ui/dialog/DeleteDialog.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/dialog/DeleteDialog.kt
@@ -1,12 +1,14 @@
 package zero.friends.gostopcalculator.ui.dialog
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import zero.friends.gostopcalculator.R
 
 @Composable
@@ -18,17 +20,24 @@ fun DeleteDialog(
         onDismissRequest = onDismiss,
         title = { Text(text = stringResource(R.string.delete_dialog_title)) },
         confirmButton = {
-            Button(onClick = onClick) {
-                Text(text = stringResource(R.string.delete))
-            }
+            Text(
+                modifier = Modifier
+                    .clickable { onClick() }
+                    .padding(10.dp),
+                text = stringResource(R.string.delete),
+                color = colorResource(
+                    id = R.color.orangey_red
+                )
+            )
         },
         dismissButton = {
-            Button(
-                onClick = onDismiss,
-                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.gray))
-            ) {
-                Text(text = stringResource(R.string.cancel), color = colorResource(id = R.color.white))
-            }
+            Text(
+                modifier = Modifier
+                    .clickable { onDismiss() }
+                    .padding(10.dp),
+                text = stringResource(R.string.cancel),
+                color = colorResource(id = R.color.orangey_red)
+            )
         }
     )
 }
