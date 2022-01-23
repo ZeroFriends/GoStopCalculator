74d702f54e6979eaf7559356ced3a5ab298045bf
zozero94 <android_develop@kakao.com>
2022-01-24 00:20:20 +0900

    Release 1.0.1

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -4,8 +4,8 @@ object Versions {
 
     const val minSdk = 24
     const val targetSdk = 30
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
 }
 
 object Dep {
