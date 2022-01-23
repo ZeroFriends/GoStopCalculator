3a1510e219413257bb63be055be3eb8d463ef623
zozero94 <android_develop@kakao.com>
2022-01-23 22:48:36 +0900

    [#18] Admob 초기화 먼저 해놓도록 변경 (광고 속도 개선)

--- app/src/main/java/zero/friends/gostopcalculator/ui/MainActivity.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package zero.friends.gostopcalculator.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import zero.friends.gostopcalculator.AdCallback
 import zero.friends.gostopcalculator.GoogleAdmob
 import zero.friends.gostopcalculator.Navigator
+import zero.friends.gostopcalculator.R
 import zero.friends.gostopcalculator.theme.GoStopTheme
 import zero.friends.gostopcalculator.util.getEntryPointFromActivity
 import javax.inject.Inject
@@ -25,9 +28,15 @@ class MainActivity : AppCompatActivity() {
                         finish()
                     },
                     showAds = { endAds ->
-                        googleAdmob.loadAds(
-                            adCallback = {
-                                endAds()
+                        googleAdmob.showAd(
+                            adCallback = { adCallback ->
+                                when (adCallback) {
+                                    AdCallback.OnError -> {
+                                        Toast.makeText(this, getString(R.string.ad_error), Toast.LENGTH_SHORT).show()
+                                        endAds()
+                                    }
+                                    AdCallback.OnSuccess -> endAds()
+                                }
                             },
                         )
                     },
