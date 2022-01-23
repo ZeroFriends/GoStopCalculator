9003f3288f8fcef5ea8f150d8f0f332435c609aa
zozero94 <android_develop@kakao.com>
2022-01-24 00:15:16 +0900

    [#18] minSdk 21 -> 24

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -2,7 +2,7 @@ object Versions {
     const val compileSdk = 31
     const val buildTools = "30.0.3"
 
-    const val minSdk = 21
+    const val minSdk = 24
     const val targetSdk = 30
     const val versionCode = 1
     const val versionName = "1.0.0"
