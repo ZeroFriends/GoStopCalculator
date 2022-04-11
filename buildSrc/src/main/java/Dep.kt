e23575ad9c5cee583e8a9fa203c503dee0d8210f
zozero94 <android_develop@kakao.com>
2022-04-11 20:25:33 +0900

    app signing 추가

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -9,7 +9,7 @@ object Versions {
 }
 
 object Dep {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.2"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.1.3"
 
     object AndroidX {
         const val core = "androidx.core:core-ktx:1.7.0"
