aa0197027ddca124aa78fce5b1a888b32d3d057b
zozero94 <zozero94@gmail.com>
2022-09-04 19:35:55 +0900

    [Fix] - 고박유저는 무조건 한명만 존재하도록 변경 #23

--- domain/src/main/java/zero/friends/domain/usecase/option/ToggleLoserOptionUseCase.kt
+++ domain/src/main/java/zero/friends/domain/usecase/option/ToggleLoserOptionUseCase.kt
@@ -1,6 +1,5 @@
 package zero.friends.domain.usecase.option
 
-import zero.friends.domain.model.Gamer
 import zero.friends.domain.model.LoserOption
 import zero.friends.domain.repository.GamerRepository
 import javax.inject.Inject
@@ -9,15 +8,23 @@ class ToggleLoserOptionUseCase @Inject constructor(
     private val gamerRepository: GamerRepository,
     private val updateOptionsUseCase: UpdateOptionsUseCase
 ) {
-    suspend operator fun invoke(gamer: Gamer, option: LoserOption) {
-        val cacheGamer = gamerRepository.getGamer(gamer.id)
-        val hasOption = cacheGamer.loserOption.contains(option)
-        val toggledList = if (hasOption) {
-            cacheGamer.loserOption - option
-        } else {
-            cacheGamer.loserOption + option
+    suspend operator fun invoke(gamerId: Long, option: LoserOption) {
+        val currentGamer = gamerRepository.getGamer(gamerId)
+        val roundGamers = gamerRepository.getRoundGamers(currentGamer.roundId)
+
+        val otherGoBakGamer =
+            (roundGamers - currentGamer).firstOrNull { it.loserOption.contains(LoserOption.GoBak) }
+
+        if (otherGoBakGamer != null) {//고박은 only 1개
+            updateOptionsUseCase(otherGoBakGamer.id, emptyList(), LoserOption.GoBak::class)
         }
-        updateOptionsUseCase(cacheGamer.id, toggledList, option::class)
+        val hasOption = currentGamer.loserOption.contains(option)
+        val toggledList = if (hasOption) {
+            currentGamer.loserOption - option
+        } else {
+            currentGamer.loserOption + option
+        }
+        updateOptionsUseCase(currentGamer.id, toggledList, option::class)
     }
 
 }
