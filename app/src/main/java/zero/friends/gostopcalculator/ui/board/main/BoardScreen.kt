88ee6f8a55d832ca419d03eee2a9a3f87f733e06
zozero94 <zozero94@gmail.com>
2022-09-09 16:34:10 +0900

    Revert "[refactor] - reverse 제거"
    This reverts commit 15f80b06

--- app/src/main/java/zero/friends/gostopcalculator/ui/board/main/BoardScreen.kt
+++ app/src/main/java/zero/friends/gostopcalculator/ui/board/main/BoardScreen.kt
@@ -200,7 +200,7 @@ private fun GameHistory(
         } else {
             Spacer(modifier = Modifier.padding(5.dp))
             LazyColumn(state = lazyListState, verticalArrangement = Arrangement.spacedBy(10.dp)) {
-                itemsIndexed(uiState.gameHistories.toList()) { index, (roundId, gamer) ->
+                itemsIndexed(uiState.gameHistories.toList().asReversed()) { index, (roundId, gamer) ->
                     RoundBox(
                         index = uiState.gameHistories.size - index - 1,
                         gamers = gamer,
