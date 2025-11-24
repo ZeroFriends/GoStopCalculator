ecdd8fd8b05883bde7c9a08079050abf83626b10
zozero94 <zozero94@gmail.com>
2025-11-24 14:25:28 +0900

    디버그 빌드 크래시 리포팅 수집하지 않도록 변경

--- app/src/main/java/zero/friends/gostopcalculator/GoStopApplication.kt
+++ app/src/main/java/zero/friends/gostopcalculator/GoStopApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import zero.friends.gostopcalculator.work.EmptyRoundCleanupWorker
 import javax.inject.Inject
@@ -21,6 +22,8 @@ class GoStopApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
+
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = !BuildConfig.DEBUG
 
         WorkManager.initialize(this, workManagerConfiguration)
         EmptyRoundCleanupWorker.enqueue(applicationContext)
